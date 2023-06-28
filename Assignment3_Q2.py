e12977c458ecc555eed6a90692a1117e242f9143
Lavanya2227 <136583131+Lavanya2227@users.noreply.github.com>
2023-06-28 12:33:20 +0530

    Add files via upload

---
+++ Assignment3_Q2.py
@@ -0,0 +1,10 @@
+
+#String inside the function
+#Reversing the given string
+
+def reverse(str):
+    result = str[::-1]
+    return result
+str = input("Enter sample string:")
+print("Reversed string is :", reverse(str))
+
